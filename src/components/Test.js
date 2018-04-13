d16353e4df43a87aa64521a0bb3b4f6bc2c6798e
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-04-13 13:11:04 +0300

    Create functional for check correct answers

--- src/components/Test.js
+++ src/components/Test.js
@@ -23,7 +23,8 @@ class Test extends Component {
 					answer: 0
 				}
 			],
-			quizAnswers: []
+			quizAnswers: [],
+			correctAnswers: []
 		}
 
 	}
@@ -31,9 +32,51 @@ class Test extends Component {
 	quizSubmit = (e) => {
 		e.preventDefault();
 
-		this.state.quizAnswers.map( (item, i) => {
-			console.log(item);
+		let amountCorrectAnswers = 0;
+
+		for (let answer_user in this.state.quizAnswers){
+			//console.log(`User Answers: ${this.state.quizAnswers[answer_user]}`);
+			console.log( `Ответы пользователя: ${this.state.quizAnswers[answer_user]}` );
+
+		}
+
+		console.log('--------------------------');
+
+		this.state.quiz.map( (question, i) => {
+			
+			let numberCorrectAnswer = question.answer;
+
+			let answers = question.answers;
+
+			console.log(`Правильные ответы на вопросы: ${answers[numberCorrectAnswer]}`);
+
+			//if(answers[numberCorrectAnswer])
+
+			
+
+			for (let answerUser in this.state.quizAnswers){
+
+				if(this.state.quizAnswers[answerUser] == answers[numberCorrectAnswer]){
+
+					amountCorrectAnswers++;
+					
+				}
+
+			}
+
 		});
+
+		console.log(`Вы правильно ответили на ${amountCorrectAnswers} вопроса.`);
+
+		// for(let answer_correct in this.state.quiz){
+		// 	let answers = this.state.quiz[answer_correct].answers;
+		// 	let number = this.state.quiz[answer_correct].answer;
+			
+		// 	console.log(`Правильные ответы на вопросы: ${answers[number]}`);
+		// }
+
+
+		
 	}
 
 
@@ -41,7 +84,7 @@ class Test extends Component {
 
 		let nextState = Object.assign({}, this.state.quizAnswers, { [e.target.name]: e.target.value} );
 		this.setState({
-			quizAnswers: nextState	
+			quizAnswers: nextState
 		});
 	}
 
@@ -55,7 +98,7 @@ class Test extends Component {
 							this.state.quiz.map( (item, i) => {
 								return(
 									<div className="quiz__item" key={'quiz-' + i}>
-										<h4>{item.question}</h4>
+										<h4>{`№${i}: ${item.question}`}</h4>
 										<div className="quiz__item-options">
 											{
 												item.answers.map( (option, answer_i) => {
@@ -64,11 +107,11 @@ class Test extends Component {
 															<input 
 																type="radio"
 																name={`quiz[${i}]`} 
-																id={i} 
+																id={`group-${i}-quiz-${answer_i}`} 
 																value={option} 
 																onChange={this.radioChange}
 																/>
-															<label htmlFor={i}>{option}</label>
+															<label htmlFor={`group-${i}-quiz-${answer_i}`}>{option}</label>
 														</p>
 													)
 												})
